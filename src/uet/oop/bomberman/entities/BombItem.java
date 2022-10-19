cea8eb7dec2861dbda9fc5902820569ef0085282
Trần Quốc Phi <100513675+21020374@users.noreply.github.com>
2022-10-19 17:24:44 +0700

    Create BombItem.java

---
+++ src/uet/oop/bomberman/entities/BombItem.java
@@ -0,0 +1,24 @@
+package uet.oop.bomberman.entities.Item;
+
+import javafx.scene.image.Image;
+import uet.oop.bomberman.entities.Entity;
+import uet.oop.bomberman.graphics.Sprite;
+
+public class BombItem extends Entity{
+    public BombItem(int x, int y, Image img) {
+        super( x, y, img);
+        super.state = "live";
+    }
+
+    public boolean collidesWithBomber(Entity bomber){
+        if ((x-bomber.getX())/ Sprite.SCALED_SIZE == 0 && (y- bomber.getY())/Sprite.SCALED_SIZE == 0) {
+            return true;
+        }
+        return false;
+    }
+    @Override
+    public void update(int time) {
+
+    }
+
+}
